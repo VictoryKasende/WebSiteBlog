d53e5f34fcd84eb042e71b0364d130d389b4a5c3
Victory <victorykasende@gmail.com>
2024-03-21 21:09:06 +0100

    commit initial

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from tinymce.models import HTMLField
 
 class Article(models.Model):
-    title = models.CharField(max_length=255)
+    #title = models.CharField(max_length=255)
     content = HTMLField()
     date = models.DateField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
